f3dec35d3268aca930d1faf15b6ee4536703a09b
rovaidkhan07 <rovaidkundi@gmail.com>
2025-07-15 19:00:12 -0700

    initial files

--- dom.js
+++
@@ -1,6 +0,0 @@
-//let heading = document.getElementById("heading"); 
-//console.log(heading);
-
-let heading = document.getElementsByClassName("heading");
-console.dir(heading);  
-console.log(heading);
